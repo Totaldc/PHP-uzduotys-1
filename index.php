2bdd2515e6073089c467c88f1344c94a58d85be6
Paulius <pauliuspk@gmail.com>
2020-06-03 12:38:00 +0300

    uzduotis #12 array_slice stringus, padauginti likusius int su for ir array_merge du masyvus atgal

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
-echo "a <br> b <br> c <br> d";
+$a = array (10,20,30, 1, 1 ,50, 0, 0, "jonas", "petras");
+
+$b = array_slice($a, 8);
+
+var_dump($b);
+
+for ($i=0;$i<8;$i++) {
+   $new[] = $a[$i] * 10;
+  }
+  
+  $result = array_merge($new, $b);
+
+  var_dump($result);
 
 ?>
